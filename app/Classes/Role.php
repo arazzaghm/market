01cad185567723f0174fd7dfcd7e67ef04045430
Vlad Kustovskyi <starch1doff@gmail.com>
2019-10-18 22:51:23 +0300

    init

---
+++ app/Classes/Role.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Classes;
+
+class Role
+{
+    const USER = 1;
+    const MODERATOR = 2;
+    const ADMIN = 3;
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('123456'),
+            'role' => \App\Classes\Role::ADMIN
+        ]);
+
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
