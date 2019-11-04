d7156b9a4e80d3bc7f8c91a1e67952c6535282a5
Vlad Kustovskyi <starch1doff@gmail.com>
2019-11-04 22:28:53 +0200

    change the view of company page

--- resources/views/pages/companies/show.blade.php
+++ resources/views/pages/companies/show.blade.php
@@ -6,10 +6,23 @@
             <h2>{{$company->name}}</h2>
         </div>
         <div class="card-body">
-            <img src="{{$company->getFirstMediaUrl('logo')}}" alt="{{$company->name}}">
-            <p>Phone number: {{$company->getFormattedPhoneNumber()}}</p>
-            <p>{{$company->description}}</p>
-            <p>Owner: <a href="{{route('users.show', ['user' => $company->user])}}"> {{$company->user->name}}</a></p>
+            <div class="row">
+                <div class="col-4">
+                    <img src="{{$company->getFirstMediaUrl('logo')}}" alt="{{$company->name}}">
+                </div>
+                <div class="col">
+                    <p>Email: @auth {{$company->email}} @else Log in or register to view the credentials @endauth</p>
+                    <p>Phone number: @auth {{$company->getFormattedPhoneNumber()}} @else Log in or register to view the
+                        credentials @endauth</p>
+                    <p>{{$company->description}}</p>
+                    <p>
+                        Owner:
+                        <a href="{{route('users.show', ['user' => $company->user])}}">
+                            {{$company->user->name}}
+                        </a>
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
