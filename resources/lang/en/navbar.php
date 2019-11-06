b6d6ab9e80867e1dd5e1a1678e7f9eaef3c66075
Vlad Kustovskyi <starch1doff@gmail.com>
2019-11-07 00:02:13 +0200

    fixup! added localization for navbar and fixed bug with null locale after logout

---
+++ resources/lang/en/navbar.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'search' => 'Search',
+    'myProfile' => 'My profile',
+    'myCompany' => 'My company',
+    'myBookmarks' => 'My bookmarks',
+    'mySettings' => 'My settings',
+    'help' => 'Help',
+    'logout' => 'Logout',
+    'login' => 'Login',
+    'register' => 'Register'
+];
---
+++ resources/lang/ru/navbar.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'search' => 'Поиск',
+    'myProfile' => 'Мой профиль',
+    'myCompany' => 'Моя компания',
+    'myBookmarks' => 'Мои закладки',
+    'mySettings' => 'Мои настройки',
+    'help' => 'Помощь',
+    'logout' => 'Выйти',
+    'login' => 'Войти',
+    'register' => 'Зарегистрироваться'
+];
