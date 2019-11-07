96a9a34c085513fb75452bad53d0fabe1b542589
Vlad Kustovskyi <starch1doff@gmail.com>
2019-11-07 23:46:00 +0200

    refactored the routes

--- routes/web.php
+++ routes/web.php
@@ -28,52 +28,71 @@ Route::get('/popular-questions', 'PopularQuestionController@index')->name('popul
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => ['auth', 'admin'], 'namespace' => 'Admin'], function () {
     Route::get('/', 'IndexController@index')->name('home');
-    Route::get('/users', 'UserController@index')->name('users.index');
-    Route::get('/users/edit/{user}', 'UserController@edit')->name('users.edit');
-    Route::post('/users/ban/{user}', 'UserController@ban')->name('users.ban');
+});
 
-    Route::get('/posts', 'PostController@index')->name('posts.index');
+Route::group(['prefix' => 'admin', 'as' => 'admin.users.', 'middleware' => ['auth', 'admin'], 'namespace' => 'Admin'], function () {
+    Route::get('/users', 'UserController@index')->name('index');
+    Route::get('/users/edit/{user}', 'UserController@edit')->name('edit');
+    Route::post('/users/ban/{user}', 'UserController@ban')->name('ban');
+});
 
-    Route::get('/categories', 'CategoryController@index')->name('categories.index');
-    Route::get('/categories/create', 'CategoryController@create')->name('categories.create');
-    Route::post('/categories/store', 'CategoryController@store')->name('categories.store');
-    Route::get('/categories/edit/{category}', 'CategoryController@edit')->name('categories.edit');
-    Route::patch('/categories/{category}', 'CategoryController@update')->name('categories.update');
-    Route::delete('/categories/delete/{category}', 'CategoryController@destroy')->name('categories.destroy');
-    Route::patch('/categories/pin/{category}', 'CategoryController@pin')->name('categories.pin');
+Route::group(['prefix' => 'admin', 'as' => 'admin.posts.', 'middleware' => ['auth', 'admin'], 'namespace' => 'Admin'], function () {
+    Route::get('/posts', 'PostController@index')->name('index');
+});
 
-    Route::get('/report-types', 'ReportTypeController@index')->name('report-types.index');
-    Route::get('/report-types/create', 'ReportTypeController@create')->name('report-types.create');
-    Route::post('/report-types/store', 'ReportTypeController@store')->name('report-types.store');
-    Route::get('/report-types/{reportType}', 'ReportTypeController@edit')->name('report-types.edit');
-    Route::patch('/report-types/{reportType}', 'ReportTypeController@update')->name('report-types.update');
-    Route::delete('/report-types/delete/{reportType}', 'ReportTypeController@destroy')->name('report-types.destroy');
+Route::group(['prefix' => 'admin', 'as' => 'admin.categories.', 'middleware' => ['auth', 'admin'], 'namespace' => 'Admin'], function () {
+    Route::get('/categories', 'CategoryController@index')->name('index');
+    Route::get('/categories/create', 'CategoryController@create')->name('create');
+    Route::post('/categories/store', 'CategoryController@store')->name('store');
+    Route::get('/categories/edit/{category}', 'CategoryController@edit')->name('edit');
+    Route::patch('/categories/{category}', 'CategoryController@update')->name('update');
+    Route::delete('/categories/delete/{category}', 'CategoryController@destroy')->name('destroy');
+    Route::patch('/categories/pin/{category}', 'CategoryController@pin')->name('pin');
+});
 
-    Route::get('/reports', 'ReportController@index')->name('reports.index');
-    Route::get('/reports/create', 'ReportController@create')->name('reports.create');
-    Route::get('/reports/show/{report}', 'ReportController@show')->name('reports.show');
-    Route::post('/reports/store', 'ReportController@store')->name('reports.store');
-    Route::patch('/reports/update/{report}', 'ReportController@update')->name('reports.update');
-    Route::delete('/reports/delete/{report}', 'ReportController@destroy')->name('reports.destroy');
+Route::group(['prefix' => 'admin', 'as' => 'admin.report-types.', 'middleware' => ['auth', 'admin'], 'namespace' => 'Admin'], function () {
+    Route::get('/report-types', 'ReportTypeController@index')->name('index');
+    Route::get('/report-types/create', 'ReportTypeController@create')->name('create');
+    Route::post('/report-types/store', 'ReportTypeController@store')->name('store');
+    Route::get('/report-types/{reportType}', 'ReportTypeController@edit')->name('edit');
+    Route::patch('/report-types/{reportType}', 'ReportTypeController@update')->name('update');
+    Route::delete('/report-types/delete/{reportType}', 'ReportTypeController@destroy')->name('destroy');
+});
 
-    Route::get('/questions', 'QuestionController@index')->name('questions.index');
-    Route::get('/questions/{question}', 'QuestionController@show')->name('questions.show');
-    Route::post('/questions/answer/{question}', 'QuestionController@answer')->name('questions.answer');
+Route::group(['prefix' => 'admin', 'as' => 'admin.reports.', 'middleware' => ['auth', 'admin'], 'namespace' => 'Admin'], function () {
+    Route::get('/reports', 'ReportController@index')->name('index');
+    Route::get('/reports/create', 'ReportController@create')->name('create');
+    Route::get('/reports/show/{report}', 'ReportController@show')->name('show');
+    Route::post('/reports/store', 'ReportController@store')->name('store');
+    Route::patch('/reports/update/{report}', 'ReportController@update')->name('update');
+    Route::delete('/reports/delete/{report}', 'ReportController@destroy')->name('destroy');
+});
 
-    Route::get('/popular-questions', 'PopularQuestionController@index')->name('popular-questions.index');
-    Route::get('/popular-questions/create', 'PopularQuestionController@create')->name('popular-questions.create');
-    Route::post('/popular-questions/store', 'PopularQuestionController@store')->name('popular-questions.store');
-    Route::get('/popular-questions/edit/{popularQuestion}', 'PopularQuestionController@edit')->name('popular-questions.edit');
-    Route::patch('/popular-questions/update/{popularQuestion}', 'PopularQuestionController@update')->name('popular-questions.update');
-    Route::delete('/popular-questions/update/{popularQuestion}', 'PopularQuestionController@destroy')->name('popular-questions.destroy');
+Route::group(['prefix' => 'admin', 'as' => 'admin.questions.', 'middleware' => ['auth', 'admin'], 'namespace' => 'Admin'], function () {
+    Route::get('/questions', 'QuestionController@index')->name('index');
+    Route::get('/questions/{question}', 'QuestionController@show')->name('show');
+    Route::post('/questions/answer/{question}', 'QuestionController@answer')->name('answer');
+});
 
-    Route::get('/currencies', 'CurrencyController@index')->name('currencies.index');
-    Route::post('/currencies/store', 'CurrencyController@store')->name('currencies.store');
-    Route::get('/currencies/edit/{currency}', 'CurrencyController@edit')->name('currencies.edit');
-    Route::patch('/currencies/update/{currency}', 'CurrencyController@update')->name('currencies.update');
-    Route::delete('/currencies/destroy/{currency}', 'CurrencyController@destroy')->name('currencies.destroy');
+Route::group(['prefix' => 'admin', 'as' => 'admin.popular-questions.', 'middleware' => ['auth', 'admin'], 'namespace' => 'Admin'], function () {
+    Route::get('/popular-questions', 'PopularQuestionController@index')->name('index');
+    Route::get('/popular-questions/create', 'PopularQuestionController@create')->name('create');
+    Route::post('/popular-questions/store', 'PopularQuestionController@store')->name('store');
+    Route::get('/popular-questions/edit/{popularQuestion}', 'PopularQuestionController@edit')->name('edit');
+    Route::patch('/popular-questions/update/{popularQuestion}', 'PopularQuestionController@update')->name('update');
+    Route::delete('/popular-questions/update/{popularQuestion}', 'PopularQuestionController@destroy')->name('destroy');
+});
 
-    Route::get('/companies', 'CompanyController@index')->name('companies.index');
+Route::group(['prefix' => 'admin', 'as' => 'admin.currencies.', 'middleware' => ['auth', 'admin'], 'namespace' => 'Admin'], function () {
+    Route::get('/currencies', 'CurrencyController@index')->name('index');
+    Route::post('/currencies/store', 'CurrencyController@store')->name('store');
+    Route::get('/currencies/edit/{currency}', 'CurrencyController@edit')->name('edit');
+    Route::patch('/currencies/update/{currency}', 'CurrencyController@update')->name('update');
+    Route::delete('/currencies/destroy/{currency}', 'CurrencyController@destroy')->name('destroy');
+});
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.companies.', 'middleware' => ['auth', 'admin'], 'namespace' => 'Admin'], function () {
+    Route::get('/companies', 'CompanyController@index')->name('index');
 });
 
 Route::group(['middleware' => ['auth'], 'as' => 'posts.'], function () {
@@ -110,13 +129,9 @@ Route::group(['middleware' => ['auth'], 'as' => 'settings.'], function () {
 
 Route::group(['middleware' => ['auth']], function () {
     Route::patch('/users/update/{user}', 'UserController@update')->name('users.update');
-
     Route::post('/users/avatar/{user}/change', 'AvatarController@store')->name('avatar.store');
-
     Route::post('/posts/{post}/picture/destroy', 'PostPictureController@destroy')->name('post-pictures.destroy');
-
     Route::post('/comment/store/{post}', 'CommentController@store')->name('comments.store');
-
     Route::post('/reports/store/{model}', 'ReportController@store')->name('reports.store');
 });
 
@@ -129,7 +144,5 @@ Route::group(['middleware' => ['auth', 'hasCompany'], 'as' => 'my-company.'], fu
 });
 
 Route::get('/companies/{company}', 'CompanyController@show')->name('companies.show');
-
 Route::get('/posts/{category?}', 'PostController@index')->name('posts.index');
-
 Route::get('lang/{locale}', 'LanguageController@set')->name('language.set');
